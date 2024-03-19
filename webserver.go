3fc82bf8d3ca3badc7c8c0b2ab6bf6ceeb640b0e
Arnold Lara <lara.arnoldandres@gmail.com>
2024-03-19 06:26:47 -0500

    Creando go templates y jugando con decodificacion json

--- webserver.go
+++ webserver.go
@@ -1,13 +1,56 @@
+// forms.go
 package main
 
 import (
-    "log"
+    "html/template"
     "net/http"
+	"fmt"
 )
 
+type ContactDetails struct {
+	Success bool
+    Email   string
+    Subject string
+    Message string
+}
+
+type city1 struct {
+    name   string
+    lat string
+    long string
+    temp int
+}
+
+type city2 struct {
+    name   string
+    lat string
+    long string
+    temp int
+}
+
+
 func main() {
+    tmpl := template.Must(template.ParseFiles("./static/index.html"))
 
-    http.Handle("/", http.FileServer(http.Dir("./static")))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
+        details := ContactDetails{
+			Success: true,
+            Email:   r.FormValue("email"),
+            Subject: r.FormValue("subject"),
+            Message: r.FormValue("message"),
+        }
+
+        // do something with details
+        _ = details
+		fmt.Println(details.Email)
+
+        tmpl.Execute(w, details)
+    })
+
+    http.ListenAndServe(":8080", nil)
 }
